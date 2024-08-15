734b9149f628874f683b0ab2fd3977c1ede61aa2
wesleygurizao <160614114+wesleygurizao@users.noreply.github.com>
2024-08-15 10:43:26 -0300

    Add files via upload

---
+++ Inteligencia-artificial-3S-rie-main/Inteligencia-artificial-3S-rie-main/inteligencia artificial/script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
